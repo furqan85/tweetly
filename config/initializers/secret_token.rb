0e7d0cb771128b8e574ac2d0f5eada37641f02d9
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-10-04 13:03:41 +0800

    initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetly::Application.config.secret_token = 'bc77edb44be5f2529f82c60f840abde7d0b9bd46f4cc8bfa6fd837a900d23ddb183535d47ab40fe4d9572078fd6e27e40fe21cf4de1452d0e457e1487c072528'
